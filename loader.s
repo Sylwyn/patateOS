b9ca1f3af53a83db1cbb5263e52c78e24605b284
Sylwyn <sylgiardi@gmail.com>
2024-06-24 05:39:44 +0200

    first push on VM

---
+++ loader.s
@@ -0,0 +1,32 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+  .long MAGIC
+  .long FLAGS
+  .long CHECKSUM
+
+.section .text
+.extern kernelMain
+.extern callConstructors
+.global loader
+
+loader:
+  mov $kernel_stack , %esp
+  
+  call callConstructors
+
+  push %eax
+  push %ebx ; # send multiboot structure and magic number to kernel main
+  call kernelMain
+
+_stop: ; # infinite loop in case kernel die
+  cli
+  hlt
+  jmp _stop
+
+.section .bss
+.space 2*1024*1024 ; # 2 MiB 
+kernel_stack: 
+
