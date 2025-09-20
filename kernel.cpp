1af5ff1542e5e14319699fe0b01157fdc7eb187e
elesteven <elouan.lesteven@gmail.com>
2025-09-20 20:19:47 +0200

    renamed printf function to print and changed behaviour when end of page is reached

--- kernel.cpp
+++ kernel.cpp
@@ -3,7 +3,7 @@
 #include "gdt.h"
 
 // Simple print fonction using 0xB8000 adress of processor to print some stuff on screen for test
-void printf(const char * str) {
+void print(const char * str) {
     static uint16_t * VideoMemory = (uint16_t *) 0xB8000;
 
     static uint8_t x = 0, y = 0;
@@ -31,11 +31,11 @@ void printf(const char * str) {
 
         if (y>=25)
         {
-            for(y=0; y<25; y++)
+            for(y=0; y<24; y++)
                 for (x=0; x<80; x++)
-                    VideoMemory[80*y+x]= ( VideoMemory[80*y+x] & 0xFF00) | ' ';
+                    VideoMemory[80*y+x]= ( VideoMemory[80*(y+1)+x]); //make all characters go up one line when writing under bottom line
             x=0;
-            y=0;
+            y=24;
 
         }
 
@@ -63,9 +63,9 @@ extern "C" void callConstructors() {
 // Have some information from bootloader in multiboot_structure and magicnumber that we keep
 extern "C" void kernelMain(void * multiboot_structure, uint32_t magicnumber) {
 
-    printf("\n");
-    printf("UwU !!\n");
-    printf("OwO !!\n");
+    print("\n");
+    print("UwU !!\n");
+    print("OwO !!\n");
 
     GlobalDescriptorTable gdt;
 
